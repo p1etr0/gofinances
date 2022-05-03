530538d1615160cb6fd39a5e1259daa8276057a0
Pietro Gonçalves <pietro@amplimed.com.br>
2022-05-03 19:08:40 -0300

    Acessando propriedades no styledComponents

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,45 @@
 import React from 'react'
+import { HighlightCard } from '../../components/HighlightCard'
 
-import {Container, Title} from './styles'
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  Icon,
+  User,
+  UserGreeting,
+  UserName,
+  HighlightCards
+} from './styles'
 
 export function Dashboard(){
 
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo 
+              source={{uri: 'https://avatars.githubusercontent.com/u/51133142?v=4'}}
+            />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Pietro Gonçalves </UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power"/>
+        </UserWrapper>
+      </Header>
+
+      <HighlightCards 
+
+      >
+        <HighlightCard /> 
+        <HighlightCard /> 
+        <HighlightCard /> 
+      </HighlightCards> 
     </Container>
   )
 }
